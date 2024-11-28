24c6d182759ddbec62d46f1485f14d88b22f2f93
Repositorio-Publico <dcrmesssi@gmail.com>
2024-11-28 09:07:15 -0500

    ajustes finales

--- presentation/components/components/MainSlideShow.tsx
+++ presentation/components/components/MainSlideShow.tsx
@@ -30,8 +30,8 @@ const MainSlideShow = ({ datos }:Props) => {
                 width={200}
                 height={350}
                 style={{width:width, height:350, justifyContent:'center', alignItems:'center'}}
-                // mode="parallax"
-                // modeConfig={{parallaxScrollingScale:0.9, parallaxScrollingOffset:50}}
+                mode="parallax"
+                modeConfig={{parallaxScrollingScale:0.9, parallaxScrollingOffset:50}}
                 defaultIndex={1}
             />
         </View>
